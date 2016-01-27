3df4e175ddba5f7b29f631030361352e7b4198e9
DerrickZhu1 <derrickzhu1@gmail.com>
2016-01-26 22:32:45 -0500

    Update Decision_Tree.py

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -9,15 +9,28 @@ data = MLUtils.loadLibSVMFile(sc, loadTrainingFilePath)
 # Split the data into training and test sets (30% held out for testing)
 (trainingData, testData) = data.randomSplit([0.7, 0.3])
 
-# Train a DecisionTree model.
+# Train a DecisionTree Classification model.
 #  Empty categoricalFeaturesInfo indicates all features are continuous.
-model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
+model1 = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
                                      impurity='gini', maxDepth=5, maxBins=32)
 
 # Evaluate model on test instances and compute test error
-predictions = model.predict(testData.map(lambda x: x.features))
-labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+predictions1 = model1.predict(testData.map(lambda x: x.features))
+labelsAndPredictions1 = testData.map(lambda lp: lp.label).zip(predictions)
 testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
 print('Test Error = ' + str(testErr))
 print('Learned classification tree model:')
-print(model.toDebugString())
+print(model1.toDebugString())
+
+# Train a DecisionTree Regression model.
+#  Empty categoricalFeaturesInfo indicates all features are continuous.
+model2 = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo={},
+                                    impurity='variance', maxDepth=5, maxBins=32)
+
+# Evaluate model on test instances and compute test error
+predictions2 = model2.predict(testData.map(lambda x: x.features))
+labelsAndPredictions2 = testData.map(lambda lp: lp.label).zip(predictions)
+testMSE = labelsAndPredictions.map(lambda (v, p): (v - p) * (v - p)).sum() / float(testData.count())
+print('Test Mean Squared Error = ' + str(testMSE))
+print('Learned regression tree model:')
+print(model2.toDebugString())
