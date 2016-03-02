bf274da9e02ac95922524eda97b7998a299d74ce
DerrickZhu1 <derrickzhu1@gmail.com>
2016-03-02 11:26:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, Response
 
 import redis
+import pycassa
+from pycassa.pool import ConnectionPool
+from pycassa.columnfamily import ColumnFamily
+from settings import KEY_SPACE
+from settings import DB_URI
+from settings import COLUMN_FAMILY
+
 
 app = Flask(__name__)
 r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
@@ -28,8 +35,15 @@ def show_basic():
 def stream():
     return Response(event_stream(), mimetype="text/event-stream")
 
+def fetch(key):
+    pool = ConnectionPool(KEY_SPACE, [DB_URI])
+    col_fam = ColumnFamily(pool, COLUMN_FAMILY)
+    col_fam.insert('row_key', {'col_name': 'col_val'})
+    return col_fam.get(str(key))
+
 
 if __name__ == '__main__':
     app.run(threaded=True,
     host='0.0.0.0'
 )
+
